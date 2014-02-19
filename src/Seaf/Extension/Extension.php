0b927554bae9c1645574ecda55c75ae87ffd8d92
Hajime MATSUMOTO <mail@hazime.org>
2014-02-19 21:02:09 +0900

    create ExtensionIF and Extension

---
+++ src/Seaf/Extension/Extension.php
@@ -0,0 +1,26 @@
+<?php
+/* vim: set expandtab ts=4 sw=4 sts=4: */
+
+/**
+ * Seaf: Simple Easy Acceptable micro-framework.
+ *
+ * Seaf Extention Base
+ *
+ * File: Seaf/Extension/Extension.php
+ * Created at: 2月 19, 2014
+ *
+ * @copyright Copyright (c) 2014, Hajime MATSUMOTO <mail@hazime.org>
+ * @license   MIT, http://mail@hazime.org
+ */
+ 
+namespace Seaf\Extension;
+
+/**
+ * Base Extension
+ *
+ * long description of Extension
+ */
+class Extension implements ExtensionIF
+{
+	
+}
