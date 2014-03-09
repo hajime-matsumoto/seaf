b7259efa98d0ff67192579c0260c0e876d9d300b
Hajime MATSUMOTO <mail@hazime.org>
2014-03-10 04:15:25 +0900

    Environmentにコメント追加

--- src/Seaf/Environment/Environment.php
+++ src/Seaf/Environment/Environment.php
@@ -4,41 +4,60 @@
  */
 namespace Seaf\Environment;
 
-use Seaf\DI\InstanceManager;
 use Seaf\DI\HelperManager;
 use Seaf\DI\ComponentManager;
-
 use Seaf\Log\Log;
 use Seaf\Helper\ArrayHelper;
 use Seaf\Commander\Command;
 
 /**
  * 環境クラス
+ * ==================================
+ *
+ * プログラムの実行環境を整えるクラス
+ *
  */
 class Environment {
 
+    /**
+     * 値の格納用
+     * @var array
+     */
     private $vars = array();
 
     /**
-     * @var InstanceManager
+     * @var HelperManager
      */
-    private $instanceManager = array();
-
+    private $helperManager = array();
 
     /**
-     *
+     * @var ComponentManager
      */
-    public function __construct ( ) {
+    private $componentManager = array();
+
+    /**
+     * インスタンスを作成
+     */
+    public function __construct ( ) 
+    {
         // ヘルパマネージャ
         $this->helperManager   = new HelperManager($this);
 
         // コンポーネントマネージャ
         $this->componentManager   = new ComponentManager($this);
 
+        // 初期化処理を呼び出す
         $this->init();
     }
 
-    public function init ( ) {
+    /**
+     * 初期化処理
+     * -----------------------
+     * * メソッドを組み込む
+     * *  
+     */
+    public function init ( ) 
+    {
 
         // ロガーを組み込む
         $this->bind('Seaf\Log\Log',array(
@@ -80,10 +99,16 @@ class Environment {
 
         // Viewを組み込む
         $this->register('view', 'Seaf\View\View', array(), function ($view) {
+
+            // レジストリの値を合わせる
             $view->vars += $this->vars;
+
+            // コンフィグオブジェクトを共有させる
             $view->register('config', function(){
-                return $this->config;
+                return $this->config();
             });
+
+            // Viewのパスを追加する
             $view->addPath(
                 realpath(implode('/', $this->getConfig(array('root.path','view.path'))))
             );
@@ -91,6 +116,8 @@ class Environment {
 
     }
 
+
+    /** 変数操作系のメソッド **/
 
     public function _set ($name, $value) 
     {
@@ -110,11 +137,25 @@ class Environment {
     }
 
 
+    /**
+     * メソッドを呼び出す
+     * ===========================
+     *
+     * 処理順序
+     * --------------------------
+     * 1. ヘルパに登録されているか探す
+     * 2. インスタンスが登録されているか探す
+     */
     public function call ($name, $params) {
+
+        // ヘルパーに登録されていれば
+        // ヘルパーに処理を委譲する
         if ($this->helperManager->isCallable($name)) {
             return $this->helperManager->call($name, $params);
         }
 
+        // コンポーネントが登録されていれば
+        // コンポーネントを取得する
         if ($this->componentManager->has($name)) {
             return Command::invoke(
                 array($this->componentManager,'get'),
@@ -125,6 +166,11 @@ class Environment {
         throw new \Exception("Invalid Call ".$name);
     }
 
+    /**
+     * メソッドを呼び出す(マジックメソッド)
+     * ===========================
+     * ::call へフォワード
+     */
     public function __call ($name, $params) {
         $return =  $this->call($name, $params);
         return $return;
