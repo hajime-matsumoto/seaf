7c0d967720ba56a97458f2e64258b35f889befcd
Hajime MATSUMOTO <mail@hazime.org>
2014-04-13 23:02:26 +0900

    全クラス書き換え

--- scripts/__bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php // vim: set ft=php ts=4 sts=4 sw=4 et:
-
-if (!defined('SEAF_PATH')) define('SEAF_PATH', realpath(__DIR__.'/../'));
-
-require_once SEAF_PATH . '/classes/Seaf.php';
--- tests/views/layout.php
+++
@@ -1,8 +0,0 @@
-<html>
-<head>
-<body>
-<?=$contents?>
-</body>
-</head>
-</html>
-
